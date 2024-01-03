c18653aea304d7806cc9e11dc9a2b5a1dff12160
amanda-dasilva <dinsil221@gmail.com>
2024-01-02 22:47:13 -0300

    made some adjustments to the AccountDropdownComponent component

--- src/components/AccountDropdownComponent.vue
+++ src/components/AccountDropdownComponent.vue
@@ -12,9 +12,9 @@
       Accounts
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <router-link class="dropdown-item" v-if="!token" :to="{ name: 'Signin' }">Log In</router-link>
-      <router-link class="dropdown-item" v-if="!token" :to="{ name: 'Signup' }">Sign Up</router-link>
-      <a class="dropdown-item" v-if="token" href="#" @click.prevent="signout">Sign Out</a>
+      <router-link class="dropdown-item" v-if="!isLoggedIn" :to="{ name: 'Signin' }">Log In</router-link>
+      <router-link class="dropdown-item" v-if="!isLoggedIn" :to="{ name: 'Signup' }">Sign Up</router-link>
+      <a class="dropdown-item" v-if="isLoggedIn" href="#" @click.prevent="signout">Sign Out</a>
     </div>
   </li>
 </template>
@@ -24,6 +24,11 @@ export default {
   name: 'AccountDropdownComponent',
   props: {
     token: Boolean,
+  },
+  computed: {
+    isLoggedIn() {
+      return this.token;
+    },
   },
   methods: {
     signout() {
