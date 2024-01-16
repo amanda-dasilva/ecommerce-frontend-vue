c5032128527262c1958206dd2a8fb65acc6f8796
amanda-dasilva <dinsil221@gmail.com>
2024-01-16 20:04:03 -0300

    Integrated save order API

--- src/views/Payment/PaymentSuccess.vue
+++ src/views/Payment/PaymentSuccess.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="alert alert-success" role="alert">
-    Payment successful</div>
+  <div class="alert alert-success" role="alert" id="message">Payment successful</div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const api = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 export default {
   name: 'PaymentSuccess',
   props: ['baseURL'],
@@ -13,7 +19,24 @@ export default {
       sessionId: null,
     };
   },
-  methods: {},
-  mounted() {},
+  methods: {
+    async saveOrder() {
+      await api
+        .post(`${this.baseURL}order/add?token=${this.token}&sessionId=${this.sessionId}`)
+        .then(() => {
+          // change message to order placed
+          document.getElementById('message').innerHTML = 'order placed';
+          //router.push()
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.token = localStorage.getItem("token");
+    this.sessionId = localStorage.getItem("sessionId");
+    this.saveOrder()
+  },
 };
 </script>
